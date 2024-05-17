b17c9ac79e5373053c9f10f8d21e01b2cfe52d0c
aubrey <168609344+dmpstr@users.noreply.github.com>
2024-05-18 07:20:07 +0800

    fix PRODUCT_STATIC_BOOT_CONTROL_HAL is obsolete
    Based on https://github.com/Anything-at-25-00/twrp_device_moondrop_MD_PH_001/commit/98508cc1cccc0b1352036b3ccb4571dace018d23

--- device.mk
+++ device.mk
@@ -16,10 +16,13 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.trinket \
+    bootctrl.trinket.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.trinket
+    bootctrl
 
 # PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.trinket \
